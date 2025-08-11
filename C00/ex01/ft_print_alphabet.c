900c8792785841b85c6ef8343e714f2d7abc552a
bezerro <beefantinelli@gmail.com>
2025-08-11 15:06:37 +0100

    added

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfantine <bfantine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 15:01:29 by bfantine          #+#    #+#             */
+/*   Updated: 2025/08/11 15:01:29 by bfantine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter < 'z')
+	{
+		write(1, &letter, 1);
+        letter++;
+	}
+}
+
+/*int	main()
+{
+	ft_print_alphabet();
+} */
